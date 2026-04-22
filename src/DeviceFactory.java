7c5b6965d6403d8e61d6209a0218aa6273ef02ba
Dejwu7 <dawid.misiak204@gmail.com>
2026-04-22 17:29:48 +0200

    Zadanie 7 (Interfejsy i Sortowanie)

--- src/DeviceFactory.java
+++ src/DeviceFactory.java
@@ -1,6 +1,6 @@
 public class DeviceFactory {
     public static SmartDevice createLivingRoomBulb(int id, String name)throws InvalidMacAddressException {
-        return new SmartDevice.Builder(id, name).withRoom("Living Room").withMacAddress("00:1A:2B:3C:4D:5E").withFirmwareVersion(2.0).build();
+        return new SmartDevice.Builder(id, name).withRoom("Living Room").withMacAddress("00:1A:2B:3C:4D:6E").withFirmwareVersion(2.0).build();
     }
 
     public static SmartDevice createBasicThermostat(int id) throws InvalidMacAddressException{
